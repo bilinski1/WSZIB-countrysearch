5409313e9e14b6ac9a8f88df30a8a2d2434ef1c4
bilinski <abc@example.com>
2021-05-09 16:00:51 +0200

    fix 2

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,38 +13,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Dataservice {
-/*  data: any; */
-/*  countries: Object; */
- // sharedcountries = this.countries;
 
 constructor(private http: HttpClient) { }
 
-/* getData(inputValue: string){
-  return this.http
-             .get("https://restcountries.eu/rest/v2/name/"+ {inputValue})
-             }
-}
- */
 getCountries(inputvalue: string):any {
   return this.http.get<any>('https://restcountries.eu/rest/v2/name/' + inputvalue)
-/*   .subscribe(response => {
-      console.log(response);
-      this.countries = response[0];
-      console.log(this.countries);
-      this.data = response as string [];
-    }
-  ); */
 }
 
 getCurrency(inputvalue: string):any {
   return this.http.get<any>('https://restcountries.eu/rest/v2/currency/' + inputvalue)
-/*   .subscribe(response => {
-      console.log(response);
-      this.data=Response;
-      console.log(this.data);
-      return response;
-    }
-  ); d*/
+}
+
+getCurrency(inputvalue: string):any {
+  return this.http.get<any>('https://restcountries.eu/rest/v2/currency/' + inputvalue)
 }
 }
 
